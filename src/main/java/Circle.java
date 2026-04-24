088ca121c76991e9d26fdd062485096156dda64b
plattine <loghindan@gmail.com>
2026-04-24 14:33:43 +0200

    first circle commit work in progres

---
+++ src/main/java/Circle.java
@@ -0,0 +1,15 @@
+public class Circle implements Shape {
+    private double radius;
+    private double circumference;
+    private double diameter;
+
+    public Circle(double radius, double circumference, double diameter) {
+        this.radius = radius;
+        this.circumference = circumference;
+        this.diameter = diameter;
+    }
+
+    public void calculateArea() {}
+
+    public void calculatePerimeter() {}
+}
