d20727e7cd7a14dbd52569e15e9c881af6192ce5
Marcus <mali1004@stud.ek.dk>
2026-04-24 15:06:08 +0200

    wde

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+public class Rectangle implements Shape {
+
+    private double height;
+    private double width
+
+            public rectangle(double height, double width {
+                this.height = height;
+                this.width = width;
+            }
+
+            @Override
+            public double calculateArea()
+            {
+                return width * height;
+            }
+
+            @Override
+            public double calculatePerimeter()
+            {
+                return 2 * (width + height);
+            }
+
+
+
+
+
+
+
+}
+
